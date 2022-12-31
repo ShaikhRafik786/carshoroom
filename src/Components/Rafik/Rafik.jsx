9b6ff9c135ca7398712967de64f21dbd2364327a
rafik  shaikh <rafikshaikhbiloli123@gmail.com>
2022-12-31 16:10:00 +0530

    add

--- src/Components/Rafik/Rafik.jsx
+++ src/Components/Rafik/Rafik.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Rafik = () => {
     return (
         <div>
-            <h1>This is main hr </h1>
+            <h1>This is main Rk</h1>
         </div>
     );
 };
